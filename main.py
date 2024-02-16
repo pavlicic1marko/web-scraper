d63c07acee1bb8bcb50d0fc4276c3197d7398227
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-02-16 17:01:55 +0100

    adding main file

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+import  threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from demo import *
 
-html = urlopen("http://www.wikipedia.org")
-bsobject = BeautifulSoup(html.read(), "html.parser")
-print(bsobject.a)
-print(bsobject.title)
+PROJECT_NAME = 'the site'
+HOME_PAGE = 'http://'
+DOMAIN_NAME = get_domain_name(HOME_PAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 4
+queue = Queue()
+Spider(PROJECT_NAME, HOME_PAGE, DOMAIN_NAME)
